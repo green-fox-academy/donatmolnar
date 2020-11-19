1b01b8bf03f6b67babe67bff217c45c7067ea0b3
donatmolnar <donatmolnar@users.noreply.github.com>
2020-11-19 14:49:22 +0100

    printing-to-terminal-window exercices added

---
+++ week-01/day-4/printing-to-terminal-window/hello-others.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+
+console.log('Hello, Csabi');
+console.log('Hello, Andi');
+console.log('Hello, Martin');
