2e84a33211d71d5cff56d468af93037792458935
donatmolnar <donatmolnar@users.noreply.github.com>
2020-11-20 12:05:29 +0100

    added loops pt1

---
+++ week-01/day-5/loops/parametic-average.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let input: number = 10;
+
+console.log('Sum: ' + (input + 1) + ', Average: ' + ((input + 1)/2));
