7a9607619349b5d3756cc81fabb3778ef9ffa3d5
donatmolnar <donatmolnar@users.noreply.github.com>
2020-11-20 20:49:56 +0100

    pyramid and diamond added

--- week-01/day-5/loops/draw-diamond.ts
+++ week-01/day-5/loops/draw-diamond.ts
@@ -14,3 +14,24 @@ let lineCount3: number = 7;
 //    *
 //
 // The diamond should have as many lines as lineCount is
+
+for (let i = 0; i < lineCount3; i++) {
+	let asterisk3: string = '';
+	if (i < lineCount3 / 2) {
+		for (let j = 1; j < lineCount3 - i; j++) {
+			asterisk3 += ' ';
+		}
+		for (let k = 1; k <= 2 * i + 1; k++) {
+			asterisk3 += '*';
+		}
+		console.log(asterisk3);
+	} else {
+		for (let j = 0; j < i; j++) {
+			asterisk3 += ' ';
+		}
+		for (let k = 1; k < (lineCount3 - i) * 2; k++) {
+			asterisk3 += '*';
+		}
+		console.log(asterisk3);
+	}
+}
