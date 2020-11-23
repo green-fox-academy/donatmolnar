a263476fceba2a70f7ee8dbdccbf7afed200d1a3
donatmolnar <donatmolnar@users.noreply.github.com>
2020-11-23 16:50:37 +0100

    tsconfig.js added

--- week-02/day-1/arrays/append-a.ts
+++ week-02/day-1/arrays/append-a.ts
@@ -1,4 +1,13 @@
+'use strict';
 
 // -  Create an array variable named `animals`
 //    with the following content: `["koal", "pand", "zebr"]`
 // -  Add all elements an `"a"` at the end
+
+let animals = ["koal", "pand", "zebr"];
+
+animals.forEach(function(): string {
+    return animals += 'a';
+})
+
+console.log(animals);
--- week-02/day-1/arrays/change-element.ts
+++ week-02/day-1/arrays/change-element.ts
@@ -1,4 +1,12 @@
+'use strict';
 
 // -  Create an array named `numbers` with the following content: `[1, 2, 3, 8, 5, 6]`
-// -  Change the 8 to 4 with the `.map` method 
+// -  Change the 8 to 4 with the `.map` method
 // -  Print the fourth element as a test
+
+let numbers = [1, 2, 3, 8, 5, 6]
+
+numbers.map(function () {
+    numbers[3] = 4});
+
+console.log(numbers[3]);
