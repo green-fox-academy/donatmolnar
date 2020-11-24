3f071d335de71609db42ea084822724622bf5c15
donatmolnar <donatmolnar@users.noreply.github.com>
2020-11-24 13:44:57 +0100

    reverse, simple-replace and folder structure added

---
+++ week-02/day-2/strings/reverse.ts
@@ -0,0 +1,16 @@
+'use strict';
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+
+let toBeReversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+
+function reverse(input: string): string {
+	let splitArray: string[] = input.split('');
+	let reversedArray: string[] = splitArray.reverse();
+	let joinedArray: string = reversedArray.join('');
+	return joinedArray;
+}
+
+console.log(reverse(toBeReversed));
+
+export = reverse;
