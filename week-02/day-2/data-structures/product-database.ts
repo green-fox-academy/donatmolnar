cc28a644b2a28ed1cd2f75e06bcaa4213383b788
donatmolnar <donatmolnar@users.noreply.github.com>
2020-11-27 14:36:22 +0100

    added product-database.ts

---
+++ week-02/day-2/data-structures/product-database.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+let products = {
+  'Eggs': '200',
+  'Milk': '200',
+  'Fish': '400',
+  'Apples': '150',
+  'Bread': '50',
+  'Chicken': '550',
+};
+
+//How much is the fish?
+console.log('The fish costs: ' + products['Fish']);
+
+//What is the most expensive product?
+
+//What is the average price?
+//How many products' price is below 300?
+//Is there anything we can buy for exactly 125?
+console.log(`${products['125']}` !== undefined);
+
+//What is the cheapest product?
