d71cfc02ef4983d52e9263cae617bedfde752f25
donatmolnar <donatmolnar@users.noreply.github.com>
2020-12-01 13:03:39 +0100

    added product-database.ts and he-will-never.ts

--- week-02/day-2/data-structures/product-database.ts
+++ week-02/day-2/data-structures/product-database.ts
@@ -20,33 +20,9 @@ Object.keys(products).forEach(key => {
     mostExpensive = current;
   } else {
     console.log('no change');
-    
   }
   console.log('Current: ' + current + ' Most expensive: ' + mostExpensive);
   })
-
-  
-
-  /*
-  function mostChars(text: string): string {
-    let characterOccurance = {};
-    for(let i: number = 0; i < text.length; i++){
-      if(characterOccurance.hasOwnProperty(text.charAt(i))){
-        characterOccurance[text.charAt(i)] += 1;
-      } else {
-        characterOccurance[text.charAt(i)] = 1;
-      }
-    }
-    let max: number = 0;
-    let maxKey: string = '';
-    Object.keys(characterOccurance).forEach(function(element){
-      if(characterOccurance[element] > max){
-        max = characterOccurance[element]
-        maxKey = element
-      }
-    })
-    return `the key is ${maxKey} and the number is ${max}`
-  }  */
 
 //What is the average price?
 //How many products' price is below 300?
