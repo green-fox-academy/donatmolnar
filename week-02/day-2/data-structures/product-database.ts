27dd037740d80b87280b32df67dc506c505e3897
donatmolnar <donatmolnar@users.noreply.github.com>
2020-12-01 13:01:54 +0100

    added product-database.ts and he-will-never.ts

--- week-02/day-2/data-structures/product-database.ts
+++ week-02/day-2/data-structures/product-database.ts
@@ -1,22 +1,56 @@
 'use strict';
 
 let products = {
-  'Eggs': '200',
-  'Milk': '200',
-  'Fish': '400',
-  'Apples': '150',
-  'Bread': '50',
-  'Chicken': '550',
+  'Eggs': 200,
+  'Milk': 200,
+  'Fish': 400,
+  'Apples': 150,
+  'Bread': 50,
+  'Chicken': 550,
 };
 
 //How much is the fish?
 console.log('The fish costs: ' + products['Fish']);
 
 //What is the most expensive product?
+let mostExpensive: number = 1;
+Object.keys(products).forEach(key => {
+  let current: number = products[key];
+  if (current > mostExpensive) {
+    mostExpensive = current;
+  } else {
+    console.log('no change');
+    
+  }
+  console.log('Current: ' + current + ' Most expensive: ' + mostExpensive);
+  })
+
+  
+
+  /*
+  function mostChars(text: string): string {
+    let characterOccurance = {};
+    for(let i: number = 0; i < text.length; i++){
+      if(characterOccurance.hasOwnProperty(text.charAt(i))){
+        characterOccurance[text.charAt(i)] += 1;
+      } else {
+        characterOccurance[text.charAt(i)] = 1;
+      }
+    }
+    let max: number = 0;
+    let maxKey: string = '';
+    Object.keys(characterOccurance).forEach(function(element){
+      if(characterOccurance[element] > max){
+        max = characterOccurance[element]
+        maxKey = element
+      }
+    })
+    return `the key is ${maxKey} and the number is ${max}`
+  }  */
 
 //What is the average price?
 //How many products' price is below 300?
 //Is there anything we can buy for exactly 125?
-console.log(`${products['125']}` !== undefined);
+//console.log(`${products['125']}` !== undefined);
 
 //What is the cheapest product?
--- week-02/day-2/strings/he-will-never.ts
+++ week-02/day-2/strings/he-will-never.ts
@@ -1,13 +1,11 @@
 'use strict';
 
-//NOT WORKING YET - PLEASE COME BACK LATER
-
 // Things are a little bit messed up
 // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
 // Assemble the fragments into the out variable
 
 let out: string = '';
-let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+let notSoCrypticMessage: number[] = [ 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11,];
 
 let hashmap = {
   7: 'run around and desert you',
@@ -17,37 +15,16 @@ let hashmap = {
   12: 'give you up, ',
   1: 'Never gonna ',
   11: '\n',
-  3: 'say goodbye '
+  3: 'say goodbye ',
 };
 
+for (let i: number = 0; i < notSoCrypticMessage.length; i++) {
+  let num: number = notSoCrypticMessage[i];
+  out += hashmap[num];
+}
 
-out += Object.keys(hashmap).forEach(key => {
-  console.log(hashmap[key]);
-})
+//out += Object.keys(hashmap).forEach(key => {
+//  console.log(hashmap[key]);
+//})
 
-
-function mapOrder(object: any, order: number[], key: string) {
-  object.sort((a: number, b: number) => {
-    let A = a[key];
-    let B = b[key];
-    return order.indexOf(A) > order.indexOf(B) ? 1 : -1;
-  });
-  return object;
-};
-
-
-console.log(mapOrder(hashmap, notSoCrypticMessage, Object.keys(hashmap[key])));
-
-//ordered_array = item_array.mapOrder(item_array, item_order, 'id');
-
-/*
-Find the values of key in an object:
-Object.keys(pistike1).forEach(key => {
- console.log(pistike1[key])
-}) 
-*/
-
-//It sorts numbered arrays 
-//console.log(notSoCrypticMessage.sort((a, b) => a - b));
-
-console.log(out)
+console.log(out);
