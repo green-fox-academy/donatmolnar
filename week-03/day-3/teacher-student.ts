4da0bcd11e63e58c649f47989c124286edd29b94
donatmolnar <donatmolnar@users.noreply.github.com>
2020-12-02 17:52:20 +0100

    added pokemon and teacher-student.ts

---
+++ week-03/day-3/teacher-student.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+class Student {
+  _teacher;
+
+  constructor() {
+    this._teacher = new Teacher;
+  }
+
+  learn(){
+    console.log('Student is learning something new.');
+  }
+
+  question(_teacher) {
+    return this._teacher.answer();
+  }
+
+}
+
+class Teacher {
+  _student;
+
+  constructor(_student){
+    this._student = new Student;
+  }
+
+  teach(_student){
+    return this._student.learn();
+  }
+
+  answer() {
+    console.log('The teacher is answering a question.');
+    
+  }
+}
+
+let student1 = new Student();
+let teacher1 = new Teacher();
+
+student1.question(teacher1);
+teacher1.teach(student1);
+
+
+
