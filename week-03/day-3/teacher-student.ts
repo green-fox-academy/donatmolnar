8c8ce1c8484868a7b1be631ca0ccecf276301668
donatmolnar <donatmolnar@users.noreply.github.com>
2020-12-03 17:28:33 +0100

    npm modules installed

--- week-03/day-3/teacher-student.ts
+++ week-03/day-3/teacher-student.ts
@@ -4,33 +4,31 @@ class Student {
   _teacher;
 
   constructor() {
-    this._teacher = new Teacher;
+    this._teacher = new Teacher();
   }
 
-  learn(){
+  learn() {
     console.log('Student is learning something new.');
   }
 
   question(_teacher) {
     return this._teacher.answer();
   }
-
 }
 
 class Teacher {
   _student;
 
-  constructor(_student){
-    this._student = new Student;
+  constructor() {
+    this._student = new Student();
   }
 
-  teach(_student){
+  teach(student) {
     return this._student.learn();
   }
 
   answer() {
     console.log('The teacher is answering a question.');
-    
   }
 }
 
@@ -39,6 +37,3 @@ let teacher1 = new Teacher();
 
 student1.question(teacher1);
 teacher1.teach(student1);
-
-
-
