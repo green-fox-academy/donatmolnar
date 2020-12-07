c0467eda13fff77990947266f0fe15f087bdd976
donatmolnar <donatmolnar@users.noreply.github.com>
2020-12-07 16:52:44 +0100

    added cohort.ts

---
+++ week-04/day-1/greenfox-inheritance/cohort.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import { Student } from './student';
+import { Mentor } from './mentor';
+
+export class Cohor {
+  protected _name: string;
+  protected _students: string[];
+  protected _mentors: string[];
+
+  constructor(name: string) {
+    this._name = name;
+    this._students = [];
+    this._mentors = [];
+  }
+
+  public addStudent(student: Student): void {
+    this._students.push();
+  }
+
+  public addMentor(mentor: Mentor): void {
+    this._mentors.push();
+  }
+
+  info(): void {
+    console.log(`The ${this._name} cohort has size of ${this._students} students 
+    and size of ${this._mentors} mentors.`);
+  }
+/*
+addStudent(Student): adds the given Student to students list
+addMentor(Mentor): adds the given Mentor to mentors list
+info(): prints out 'The name cohort has size of students students and size of mentors mentors.'*/
+
+}
