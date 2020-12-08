3ad9a3cdaf41914683718e2a73231de92d01d9d4
donatmolnar <donatmolnar@users.noreply.github.com>
2020-12-08 20:12:45 +0100

    implemented flyable

--- week-04/day-2/animals/bird.ts
+++ week-04/day-2/animals/bird.ts
@@ -1,14 +1,26 @@
 'use strict';
 
+import { Flyable } from '../flyable/flyable';
 import { Animal } from './animal';
 
-export class Bird extends Animal {
-
+export class Bird extends Animal implements Flyable {
   constructor(name: string, age?: number) {
-    super(name, age); 
+    super(name, age);
   }
 
   public breed(): string {
     return 'laying eggs.';
+  }
+
+  public land(): void {
+    console.log('Rapidly decreasing altitude.');
+  }
+
+  public fly(): void {
+    console.log('I believe I can.');
+  }
+
+  public takeOff() {
+    console.log('Fasten your seatbelts.');
   }
 }
