41b5d6ab774a0b1bb873b774e325a9e95be1bf48
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-06 18:44:16 +0100

    greenfox-logo.js finished

---
+++ week-06/day-3/greenfox-logo/greenfox-logo.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Select left and right eyes
+// They have #left-eye and #right-eye
+// turn their color (fill) to tomato
+
+const leftEye = document.getElementById('left-eye');
+const rightEye = document.getElementById('right-eye');
+
+leftEye.setAttribute('fill', 'tomato');
+rightEye.setAttribute('fill', 'tomato');
