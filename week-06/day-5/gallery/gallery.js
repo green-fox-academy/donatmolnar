680c19136caaae93613216757ff5696af2b0ae17
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-08 12:26:21 +0100

    player layout added

---
+++ week-06/day-5/gallery/gallery.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const images = [
+  './src/space-1.jpg', 
+  './src/space-2.jpg',
+  './src/space-3.jpg',
+  './src/space-4.png',
+  './src/space-5.jpg',
+  './src/space-6.jpg',
+  './src/space-7.jpg',
+  './src/space-8.png',]
+
+  
