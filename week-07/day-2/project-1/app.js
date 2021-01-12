b3cffdefc85520d98c32b12dd42df2026dcefaa7
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-12 17:38:07 +0100

    formatted app.js

--- week-07/day-2/project-1/app.js
+++ week-07/day-2/project-1/app.js
@@ -8,49 +8,49 @@ const bodyParser = require('body-parser');
 app.use(express.static('assets'));
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.get('/doubling', (req,res) => {
+app.get('/doubling', (req, res) => {
   let received = req.query.input;
   if (received === undefined) {
-    res.json({ "error": "Please provide an input!"});
+    res.json({ error: 'Please provide an input!' });
   } else {
     let result = received * 2;
-    res.json({'received': Number(received), 'result': result});
+    res.json({ received: Number(received), result: result });
   }
 });
 
-app.get('/greeter', (req,res) => {
+app.get('/greeter', (req, res) => {
   let name = req.query.name;
   let title = req.query.title;
   if (name === undefined && title === undefined) {
     res.status(400);
-    res.json({"error": "Please provide a name and a title!"})
+    res.json({ error: 'Please provide a name and a title!' });
   } else if (name === undefined) {
     res.status(400);
-    res.json({"error": "Please provide a name!"});
+    res.json({ error: 'Please provide a name!' });
   } else if (title === undefined) {
     res.status(400);
-    res.json({"error": "Please provide a title!"});
+    res.json({ error: 'Please provide a title!' });
   } else {
-    res.json({"welcome_message": `Oh, hi there ${name}, my dear ${title}!`});
+    res.json({ welcome_message: `Oh, hi there ${name}, my dear ${title}!` });
   }
 });
 
-app.get('/appenda/:appendable', (req,res) => {
+app.get('/appenda/:appendable', (req, res) => {
   let appended = req.params.appendable + 'a';
-  res.json({'appended': `${appended}`})
+  res.json({ appended: `${appended}` });
 });
 
-app.post('/dountil/:action', (req,res) => {
+app.post('/dountil/:action', (req, res) => {
   let action = req.params.action;
   let number = req.body.until;
 
   if (number == undefined) {
-    res.json({"error": "Please provide a number!"})
-  } else {  
+    res.json({ error: 'Please provide a number!' });
+  } else {
     if (action == 'sum') {
       function sum(input) {
         let sum = 0;
@@ -59,8 +59,7 @@ app.post('/dountil/:action', (req,res) => {
         }
         return sum;
       }
-      res.json({'result': Number(sum(number))});
-      
+      res.json({ result: Number(sum(number)) });
     } else if (action == 'factor') {
       function factor(input) {
         let facto = 1;
@@ -69,7 +68,7 @@ app.post('/dountil/:action', (req,res) => {
         }
         return facto;
       }
-      res.json({'result': Number(factor(number))});
+      res.json({ result: Number(factor(number)) });
     }
   }
 });
