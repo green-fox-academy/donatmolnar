f9b87852547f125975cf0c84e96bfec0d5f75890
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-12 17:36:08 +0100

    do-until feature implemented

--- week-07/day-2/project-1/app.js
+++ week-07/day-2/project-1/app.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.static('assets'));
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, './index.html'));
@@ -40,6 +42,36 @@ app.get('/greeter', (req,res) => {
 app.get('/appenda/:appendable', (req,res) => {
   let appended = req.params.appendable + 'a';
   res.json({'appended': `${appended}`})
+});
+
+app.post('/dountil/:action', (req,res) => {
+  let action = req.params.action;
+  let number = req.body.until;
+
+  if (number == undefined) {
+    res.json({"error": "Please provide a number!"})
+  } else {  
+    if (action == 'sum') {
+      function sum(input) {
+        let sum = 0;
+        for (let i = 0; i <= input; i++) {
+          sum += i;
+        }
+        return sum;
+      }
+      res.json({'result': Number(sum(number))});
+      
+    } else if (action == 'factor') {
+      function factor(input) {
+        let facto = 1;
+        for (let i = 1; i <= input; i++) {
+          facto *= i;
+        }
+        return facto;
+      }
+      res.json({'result': Number(factor(number))});
+    }
+  }
 });
 
 app.listen(3000);
