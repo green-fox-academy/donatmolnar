4475dffb872207bb88e0b8b764cabb189e4a2f16
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-12 16:02:06 +0100

    doubling feature implemented

--- week-07/day-2/project-1/app.js
+++ week-07/day-2/project-1/app.js
@@ -8,7 +8,16 @@ app.use(express.static('assets'));
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, './index.html'));
+});
 
-})
+app.get('/doubling', (req,res) => {
+  let received = req.query.input;
+  if (received === undefined) {
+    res.json({ "error": "Please provide an input!"});
+  } else {
+    let result = received * 2;
+    res.json({'received': Number(received), 'result': result});
+  }
+});
 
 app.listen(3000);
