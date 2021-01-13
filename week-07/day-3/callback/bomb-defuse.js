5944bf2aa20e6c4d73a07efb93b2a90b94ac2706
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-13 20:16:17 +0100

    bomb-defuse exercise files added

---
+++ week-07/day-3/callback/bomb-defuse.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Create a timeout that will set the display to "Bomb exploded" in 10 seconds
+// If you click on the button set the display to "Bomb defused" and stop the timeout
+// Extra: create an interval which will update the remaining seconds in the display
+    
