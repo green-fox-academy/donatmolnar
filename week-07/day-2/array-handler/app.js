7121cb187b907d884008965c2551dcf00c44fd4a
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-13 08:06:25 +0100

    sum, multiply and double feature implemented

--- week-07/day-2/array-handler/app.js
+++ week-07/day-2/array-handler/app.js
@@ -4,23 +4,33 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const { userInfo } = require('os');
 
 app.use(bodyParser.json());
 
-app.post('/arrays', (req,res) => {
+app.post('/arrays', (req, res) => {
   let action = req.body.what;
   let numberList = req.body.numbers;
 
   if (action === undefined || numberList === undefined) {
-    res.json({"error": "Please provide what to do with the numbers!"})
-  }
-  if (action = 'sum') {
-
-  } else if (action = 'multiply') {
-
-  } else if (action = 'double') {
-
+    res.json({'error': 'Please provide what to do with the numbers!' });
+  } else if (action === 'sum') {
+    let sum = 0;
+    numberList.forEach((num) => {
+      sum += num;
+    });
+    res.json({'result': sum });
+  } else if (action === 'multiply') {
+    let result = 1;
+    numberList.forEach((num) => {
+      result *= num;
+    });
+    res.json({'result': result });
+  } else if (action === 'double') {
+    let newNumberList = [];
+    numberList.forEach((num) => {
+      newNumberList.push(num * 2);
+    });
+    res.json({'result': newNumberList});
   }
 });
 
