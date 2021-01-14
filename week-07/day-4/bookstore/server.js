5ce7f8542fbed41e4ccc172bcb3bfc1ebd852d69
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-14 14:36:58 +0100

    list of titles part finished

--- week-07/day-4/bookstore/server.js
+++ week-07/day-4/bookstore/server.js
@@ -14,7 +14,28 @@ let conn = mysql.createConnection({
 });
 
 conn.connect((err) => {
-  if (err) trhow (err);
+  if (err) throw err;
   console.log('Connected to mysql');
 });
 
+app.get('/', (req,res) => {
+  res.json({Status: "Connection established: postman has been reached by the server"});
+});
+
+app.get('/titles', (req,res) => {
+  conn.query('SELECT * FROM book_mast;', (err, rows) => {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).json({error: 'error during reading sql db'})
+      return;
+    }
+    let titles = [];
+    rows.forEach((element) => {
+      titles.push(element.book_name);
+    });
+    res.json({titles});
+  });
+});
+
+
+app.listen(3000);
