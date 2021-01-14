98901434f8e407f462ad12b985ee5218ebcbaf6b
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-14 23:26:04 +0100

    filtering implemented

--- week-07/day-4/bookstore/server.js
+++ week-07/day-4/bookstore/server.js
@@ -25,14 +25,46 @@ app.get('/', (req,res) => {
   res.json({Status: "Connection established: postman has been reached by the server"});
 });
 
+let category = '';
+let publisher ='';
+let plt = 0;
+let pgt = 0;
+
 app.get('/books', (req,res) => {
-  //let category = req.query.category;
-  //let publisher = req.query.publisher;
+  if (req.query.category === undefined) {
+    category = '%';
+  } else {
+    category = `%${req.query.category}%`;
+  }
+
+  if (req.query.publisher === undefined) {
+    publisher = '%';
+  } else {
+    publisher = `%${req.query.publisher}%`;
+  }
+
+  if (req.query.plt === undefined || req.query.pgt === NaN) {
+    plt = 999999999;
+  } else {
+    plt = req.query.plt;
+  }
+
+  if (req.query.pgt === undefined || req.query.pgt === NaN) {
+    pgt = 0;
+  } else {
+    pgt = req.query.pgt;
+  }
+
   conn.query(
     `SELECT book_name, aut_name, cate_descrip, pub_name, book_price
     FROM book_mast LEFT JOIN author ON book_mast.aut_id = author.aut_id
     LEFT JOIN category ON category.cate_id = book_mast.cate_id
-    LEFT JOIN publisher ON publisher.pub_id = book_mast.pub_id;`, (err, rows) => {
+    LEFT JOIN publisher ON publisher.pub_id = book_mast.pub_id
+    WHERE cate_descrip LIKE ?
+    AND pub_name LIKE ?
+    AND book_price < ?
+    AND book_price > ?;`, [category, publisher, plt, pgt], (err, rows) => {
+    
     if (err) {
       console.log(err.toString());
       res.status(500).json({error: 'error during reading sql db'})
