0203c79109292781c26df6fda368bd7c9140d8d6
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-15 11:30:34 +0100

    index.html added

--- week-07/day-5/yt-app/server.js
+++ week-07/day-5/yt-app/server.js
@@ -2,7 +2,10 @@
 
 const express = require('express');
 const app = express();
-const mysql = mysql();
+const mysql = require('mysql');
+app.use(express.json());
+app.use(express.static('public'));
+
 
 const conn = mysql.createConnection({
   host: 'localhost',
@@ -13,29 +16,40 @@ const conn = mysql.createConnection({
 });
 
 conn.connect((err) => {
-  if(err) {
+  if (err) {
     console.log(err.toSting());
   }
   console.log('connected to mysql');
-})
+});
 
-app.get('/', (req,res) => {
-  res.send('connection established')
-})
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
-app.get('/songs', (req,res) => {
+app.get('/songs', (req, res) => {
   conn.query('SELECT * FROM songs', (err, rows) => {
-    if(err) {
+    if (err) {
       res.status(500).json(err);
     }
     res.status(200).json(rows);
   });
 });
 
+app.post('/addsong', (req, res) => {
+  conn.query(
+    `INSERT INTO songs (title, artist, url) VALUES (?,?,?);`,
+    [req.body.title, req.body.artist, req.body.url],
+    (err) => {
+      if (err) {
+        res.status(500).json(err);
+        return;
+      }
+      res.send('Sikerult');
+    }
+  );
+});
+
 app.listen(3000);
-
-
-
 
 // mysql> CREATE TABLE `songs` (
 // -> `id` INT NOT NULL AUTO_INCREMENT,
