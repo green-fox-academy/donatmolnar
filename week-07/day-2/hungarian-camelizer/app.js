57159687a8375cdc475b2c002de50cc5fd6bcdc7
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-16 15:12:42 +0100

    removing dead code

--- week-07/day-2/hungarian-camelizer/app.js
+++ week-07/day-2/hungarian-camelizer/app.js
@@ -10,8 +10,6 @@ app.post('/translate', (req,res) => {
   let text = req.body.text;
   let vowels = ['a', 'á', 'e', 'é', 'i', 'í', 'o', 'ó', 'ö', 'ő', 'u', 'ú', 'ü', 'ű', 'A', 'Á', 'E', 'É', 'I', 'Í', 'O', 'Ó', 'Ö', 'Ő', 'U', 'Ú', 'Ü', 'Ű'];
   
-  //text = text.replace(/A/g, 'A' + 'v' + 'a');
-
   vowels.forEach((vowel) => {
     text = text.replace(new RegExp(vowel, 'g' ), `${vowel}v${vowel.toLowerCase()}`);
   });
