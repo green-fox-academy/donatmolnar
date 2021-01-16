1dd576a299976db4d5d3466cac81a63d8f9bcffe
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-16 15:11:26 +0100

    hungarian-camelizer finished

--- week-07/day-2/hungarian-camelizer/app.js
+++ week-07/day-2/hungarian-camelizer/app.js
@@ -2,19 +2,20 @@
 
 const express = require('express');
 const app = express();
-//const path = require('path');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
 app.post('/translate', (req,res) => {
   let text = req.body.text;
-  let vowel = [a, á, e, é, i, í, o, ó, ö, ő, u, ú, ü, ű];
-/*
-  for (let i = 0; i < vowel.length; i++) {
-    text.replace(vowel[i], vowel[i] + 'v' + vowel[i]);
-  }
-*/
+  let vowels = ['a', 'á', 'e', 'é', 'i', 'í', 'o', 'ó', 'ö', 'ő', 'u', 'ú', 'ü', 'ű', 'A', 'Á', 'E', 'É', 'I', 'Í', 'O', 'Ó', 'Ö', 'Ő', 'U', 'Ú', 'Ü', 'Ű'];
+  
+  //text = text.replace(/A/g, 'A' + 'v' + 'a');
+
+  vowels.forEach((vowel) => {
+    text = text.replace(new RegExp(vowel, 'g' ), `${vowel}v${vowel.toLowerCase()}`);
+  });
+
   res.json({'translated': text});
 });
 
