19883c68f07f7d24a1ab409c59cc504a8670dcd9
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-17 17:36:25 +0100

    create list function implemented

--- week-07/day-3/xhr/swapi/index.js
+++ week-07/day-3/xhr/swapi/index.js
@@ -5,7 +5,15 @@ window.onload = () => {
   searchRequest.open('Get', 'https://swapi.dev/api/people');
   searchRequest.send();
   
-  searchRequest.onload = () => {
+  searchRequest.onload = (result) => {
+    console.log(result);
 
   }
 }
+
+let createList = (input) => {
+  let container = document.getElementById('results');
+  let newListItem = document.createElement('li');
+  newListItem.textContent = input;
+  container.push(newListItem);
+};
