572d0928c7e1d49f7fcad20453e126b24d1cb8e9
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-19 21:04:28 +0100

    create candy and buy lollypop implemented

---
+++ week-08/day-2/candyshop/game.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// - Gather 10.000 candies!
+// - Clicking the ‘Create candies’ button gives you 1 candy.
+// - You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
+// - 10 lollipops generate 1 candy per second.
+//   - Use the 🍭 emoji to display the lollipops you have
+// - Display the candy producton rate in the `Candies / Second` row
+// - If you press the "make candy rain" button, the candy generation should speed up 10x
+
+
+// get the elements
+let candies = document.querySelector('.candies');
+let lollypops = document.querySelector('.lollypops');
+let createCandies = document.querySelector('.create-candies');
+let buyLollypops = document.querySelector('.buy-lollypops');
+let candyMachine = document.querySelector('.candy-machine');
+let speed = document.querySelector('.speed');
+
+createCandies.addEventListener('click', () => {
+  candies.textContent++;
+})
+
+buyLollypops.addEventListener('click', () => {
+  if (candies.textContent >= 10) {
+    candies.textContent -= 10;
+    lollypops.textContent = lollypops.textContent + '🍭';
+  } else {
+    alert(`You don't have enough candies yet. Create candies to buy lollypops. 
+    10 candies = 1 lollypop`);
+  }
+});
+
+
