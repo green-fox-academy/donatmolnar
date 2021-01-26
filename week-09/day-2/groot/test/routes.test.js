326f6a976e0492a7b957326ba3a888b63bb5d382
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-26 15:33:32 +0100

    routes.test finished

--- week-09/day-2/groot/test/routes.test.js
+++ week-09/day-2/groot/test/routes.test.js
@@ -3,27 +3,27 @@
 const request = require('supertest');
 const { expect } = require('chai');
 const app = require('../routes');
-​
-describe('Groot translator', () => {
-  it('GET /groot returns received and translated', () => {
-    // TODO: implement it
-    request(app)
-    .get('/groot')
-    .send({"message": "Someting"})
-    .expect(200)
-    .end((err, res) => {
-      expect(err).to.be.null;
-      expect(res.body[0].translated).to.equal('I am Groot!');
-    });
-  });
 
-  it('GET /groot returns error when message is undefined', () => {
-    request(app)
-    .get('/groot')
-    .expect(400)
-    .end((err, res) => {
-      expect(err).to.be.null;
-      expect(res.body[0].error).to.be.equal('I am Groot!');
+describe('Groot translator', () => {
+   it('GET /groot returns received and translated', () => {
+     request(app)
+     .get('/groot')
+     .send({"message": "Someting"})
+     .expect(200)
+     .end((err, res) => {
+         if(err) {
+           expect(400);
+         } else {
+           expect(res.body[0].translated).to.equal('I am Groot!');
+         }
+     });
     });
-  });
+
+   it('GET /groot returns error when message is undefined', () => {
+     request(app)
+     .get('/groot')
+     .end((err, res) => {
+       expect(400, {"error": "I am Groot!"});
+     });
+   });
 });
