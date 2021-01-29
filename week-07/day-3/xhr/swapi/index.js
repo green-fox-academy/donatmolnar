6f4490d657d237aa7d2d78cef12906f96023ce1e
donatmolnar <donatmolnar@users.noreply.github.com>
2021-01-30 00:18:35 +0100

    swapi finished

--- week-07/day-3/xhr/swapi/index.js
+++ week-07/day-3/xhr/swapi/index.js
@@ -2,17 +2,36 @@
 
 window.onload = () => {
   let searchRequest = new XMLHttpRequest();
-  searchRequest.open('Get', 'https://swapi.dev/api/people/');
+  searchRequest.open('GET', 'https://swapi.dev/api/people/');
   searchRequest.send();
-  
+
+  let button = document.querySelector('button');
+  let inputField = document.querySelector('input');
+
   searchRequest.onload = (result) => {
-    let resultArray = JSON.parse(result.target.response).results;
-    let name = resultArray.filter(element => element.name === 'Luke Skywalker'); //Luke Skywalker
-    let films = resultArray[0].films; //example result: http://swapi.dev/api/films/1/
-    films.forEach((element) => {
-    createList(element);
-  }) ;   
-  console.log(name);
+    
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      let input = inputField.value; 
+      
+      let resultArray = JSON.parse(result.target.response).results;
+      let name = resultArray.filter(element => element.name === input);
+      let index = resultArray.indexOf(name[0]);
+      let films = resultArray[index].films; //example result: http://swapi.dev/api/films/1/
+      films.forEach((element) => {
+        
+        let titleRequest = new XMLHttpRequest();
+        titleRequest.open('GET', element);
+        titleRequest.send();
+        
+        titleRequest.onload = (result) => {
+          let titleArray = JSON.parse(result.target.response).title;
+          console.log(titleArray);
+          createList(titleArray);
+        };
+      })
+      inputField.value = '';
+  });   
   };
 };
 
