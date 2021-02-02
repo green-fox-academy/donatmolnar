2900732be21643ab5222f6c90cca2c263c48cda9
donatmolnar <donatmolnar@users.noreply.github.com>
2021-02-02 20:02:22 +0100

    list all playlists, add new playlist, delete playlist endpoints implemented

--- fox-player/server.js
+++ fox-player/server.js
@@ -23,6 +23,82 @@ conn.connect((err) => {
 
 // sends the landingpage
 app.get('/', (req,res) => {
-  res.status(200).sendFile('index.html');
+  res.status(200).sendFile('foxplayer.html');
 });
+
+let playlists = [
+  { "id": 1, "title": "Favorites", "system": 1 },
+  { "id": 2, "title": "Music for programming", "system": 0 },
+  { "id": 3, "title": "Driving", "system": 0 },
+  { "id": 5, "title": "Fox house", "system": 0 },
+];
+
+// lists all playlists
+app.get('/playlists', (req,res) => {
+  conn.query(
+    `SELECT * FROM playlists;`, (err, rows) => {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).json({error: 'error during reading sql db'});
+        return;
+      }
+      res.setHeader('Content-type', 'application/json');
+      res.status(200).json(playlists);
+    });
+});
+
+app.post('/playlist', (req,res) => {
+  let playlist = req.body.playlist;
+
+  conn.query(
+    `INSERT INTO playlists (title) VALUES ?;`, [playlist], (err, rows) => {
+      if (err) {
+        res.status(500).json(err);
+        return;
+      }
+    });
+
+    conn.query(
+      `SELECT * FROM playlists ORDER BY id;`, (err, rows) => {
+        if (err) {
+          console.log(err.toString());
+          res.status(500).json({error: 'error during reading sql db'});
+          return;
+        }
+        res.status(200).json(rows);
+      });
+});
+
+// deletes playlist based on id if it is not system playlist
+app.delete('/playlists/:id', (req, res) => {
+  let providedId = req.params.id;
+  let systemPlaylist;
+  
+  conn.query(
+    `SELECT * FROM playlists WHERE id = ?`, [id], (err, rows) => {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).json({error: 'error during reading sql db'});
+        return;
+      }
+      systemPlaylist = rows[0].system;
+    }
+  )
+
+  if (systemPlaylist === 1) {
+    res.json({error: 'System playlists cannot be removed'});
+  } else {
+    conn.query(
+      `DELETE FROM playlists WHERE id = ?;`, [id], (err, rows) => {
+        if (err) {
+          console.log(err.toString());
+          res.status(500).json({error: 'error during deleting playlist from sql db'});
+          return;
+        }
+        res.status(200).json(rows);
+    })
+  }
+})
+
+
 
