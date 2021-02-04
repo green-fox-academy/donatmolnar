ce13ade96023d54ac5bc39c35948d3ca351406f4
donatmolnar <donatmolnar@users.noreply.github.com>
2021-02-04 18:28:58 +0100

    form event listener added

---
+++ week-07/day-3/xhr/ip-location/public/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+    
+  let providedIp = document.querySelector('#ip').value;
+
+});
